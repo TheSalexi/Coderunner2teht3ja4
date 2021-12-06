f4564aa812b697d2bb737b5388059ba1fb2de648
TheSalexi <90855988+TheSalexi@users.noreply.github.com>
2021-12-06 13:03:07 +0200

    Viimeinen vaihe
    Tuotiin scanneri mainiin jotta voidaan kysellä käyttäjältä arvoja pituuteen ja leveyteen jonka jälkeen syötetyt arvot käytetään areaCalc methodissa ja palautetaan lopuksi pinta-ala.

--- src/teht3ja4.java
+++ src/teht3ja4.java
@@ -1,8 +1,18 @@
 
+import java.util.Scanner;
+
 public class teht3ja4 {
 
 	public static void main(String[] args) {
 		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Width?");
+		int widthinput = Integer.parseInt(in.nextLine());
+		System.out.println("Lenght?");
+		int lenghtinput = Integer.parseInt(in.nextLine());
+		Utils.areaCalc(widthinput, lenghtinput);
+		System.out.println("Area is " + Utils.areaCalc(widthinput, lenghtinput));
 
 	}
 
