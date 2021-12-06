524199ae1bd567b7af16ea851db8813bcc68d0dd
TheSalexi <90855988+TheSalexi@users.noreply.github.com>
2021-12-06 12:59:32 +0200

    Ensimmäinen vaihe
    Utilssin ensimmäinen methodi

---
+++ src/teht3ja4.java
@@ -0,0 +1,19 @@
+
+public class teht3ja4 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+class Utils{
+	
+	public static void printTitle()
+	{
+		System.out.println("*******");
+		System.out.println("* OOP *");
+		System.out.println("*******");
+	}
+	 
+}
